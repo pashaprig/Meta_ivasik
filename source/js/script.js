83c0d60bfcbd9e377015a36e8fc0b16db846f534
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2024-06-25 15:58:59 +0300

    WIP almost)

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,21 @@ class App {
   constructor() {
     this.step = 0
 
+    this.header = document.querySelector('.modal__header')
+    this.loader = document.querySelector('[aria-modal="loader"]');
+    this.btnSoundOn = document.querySelector('[aria-sound="on"]')
+
     this.form = document.querySelector('[aria-modal="form"]')
-    this.inputs = this.form.querySelectorAll('input:not([type="hidden"])')
-
+    this.formHeading = this.form.querySelector('.form__heading')
     this.buttonSbm = this.form.querySelector('[aria-input="submitBtn"]')
-
     this.firstname = this.form.querySelector('[aria-input="firstname"]')
     this.lastname = this.form.querySelector('[aria-input="lastname"]')
+    this.hearAbout = this.form.querySelector('[aria-input="hearAbout"]')
+    this.income = this.form.querySelector('[aria-input="income"]')
+    this.howOften = this.form.querySelector('[aria-input="howOften"]')
     this.email = this.form.querySelector('[aria-input="email"]')
-
-    this.loader = document.querySelector('[aria-modal="loader"]');
+    this.iti = this.form.querySelector('.iti')
+    this.phone1 = this.form.querySelector('[aria-input="phone1"]')
 
     this.videos = document.querySelectorAll('[aria-video*="video"]');
     this.video1 = document.querySelector('[aria-video="video-1"]');
@@ -22,9 +27,6 @@ class App {
     this.video5 = document.querySelector('[aria-video="video-5"]');
     this.video6 = document.querySelector('[aria-video="video-6"]');
     this.video7 = document.querySelector('[aria-video="video-7"]');
-
-    this.btnSoundOn = document.querySelector('[aria-sound="on"]')
-    this.header = document.querySelector('.modal__header')
   }
 
   init() {
@@ -34,22 +36,22 @@ class App {
     this.hendleNextBtnClick();
   }
 
-  setBtnColor(inputsWrapper) {
-    const inputs = inputsWrapper.map((iw) => iw.childNodes[1])
+  toggleBtnDisabled(button, shouldDisable) {
+    button.disabled = shouldDisable;
+  }
 
-    const toggleBtnDisabled = (button, shouldDisable) => {
-      button.disabled = shouldDisable;
-    };
+  setBtnColor(inputs) {
+    this.toggleBtnDisabled(this.buttonSbm, false)
 
     inputs.forEach(input => {
       input.addEventListener('keyup', () => {
         const hasError = Array.from(inputs).some(input => input.classList.contains('error'));
-        toggleBtnDisabled(this.buttonSbm, hasError);
+        this.toggleBtnDisabled(this.buttonSbm, hasError);
       });
 
       input.addEventListener('blur', () => {
         const hasError = Array.from(inputs).some(input => input.classList.contains('error'));
-        toggleBtnDisabled(this.buttonSbm, hasError);
+        this.toggleBtnDisabled(this.buttonSbm, hasError);
       });
     });
   }
@@ -66,15 +68,26 @@ class App {
     const checkStep = (step) => {
       switch (step) {
         case 1:
-          console.log('Шаг 1');
           this.step1()
           break;
         case 2:
-          console.log('Шаг 2');
+          this.step2()
           break;
         case 3:
-          console.log('Шаг 3');
+          this.step3()
           break;
+        case 4:
+          this.step4()
+          break;
+        case 5:
+          this.step5()
+          break;
+        case 6:
+          this.step6()
+          break;
+        // case 7:
+        //   this.step7()
+        //   break;
         default:
           console.log('Неизвестный шаг');
           break;
@@ -84,9 +97,9 @@ class App {
     this.buttonSbm.addEventListener('click', () => { checkStep(this.step) })
   }
 
-
   step0() {
-    this.setBtnColor([this.firstname, this.lastname]);
+    const inputs = [this.firstname, this.lastname].map((iw) => iw.childNodes[1])
+    this.setBtnColor(inputs);
 
     setTimeout(() => {
       this.header.textContent = "Соединение успешно установлено";
@@ -121,16 +134,185 @@ class App {
       }, 0);
 
     } else {
-
-
       this.hide(this.form)
       this.show(this.video2)
       this.play(this.video2);
-      this.afterVideoEnd(this.video2, [this.video2,], [this.form, this.firstname, this.lastname]);
+      this.formHeading.textContent = "Вы слышали о нейронной сети для заработка от компании Meta?"
+      this.afterVideoEnd(this.video2, [this.video2, this.firstname, this.lastname], [this.form, this.hearAbout]);
 
       this.step = 2
       this.buttonSbm.setAttribute('step', this.step)
     }
+  }
+
+  step2() {
+    const radios = this.hearAbout.querySelectorAll('input[type="radio"]')
+
+    radios.forEach(radio => {
+      radio.addEventListener('click', () => {
+        const isCheckedAbout = Array.from(radios).some(radio => radio.checked);
+        this.toggleBtnDisabled(this.buttonSbm, !isCheckedAbout);
+      });
+    });
+
+    const getSelectedRadioButton = () => {
+      for (let radio of radios) {
+        if (radio.checked) {
+          return radio;
+        }
+      }
+      return null;
+    };
+
+    if (Array.from(radios).some(radio => radio.checked)) {
+      const selectedRadio = getSelectedRadioButton();
+
+      this.hide(this.form)
+      this.step = 3
+      this.buttonSbm.setAttribute('step', this.step)
+      this.formHeading.textContent = "Сколько вы хотите получать в месяц вместе с Meta?"
+
+      if (selectedRadio.value === 'yes') {
+        this.show(this.video3a)
+        this.play(this.video3a);
+        this.afterVideoEnd(this.video3a, [this.video3a, this.hearAbout], [this.form, this.income]);
+
+      } else {
+        this.show(this.video3b)
+        this.play(this.video3b);
+        this.afterVideoEnd(this.video3b, [this.video3b, this.hearAbout], [this.form, this.income]);
+      }
+
+    } else {
+      setTimeout(() => {
+        this.buttonSbm.disabled = true;
+      }, 0);
+    }
+  }
+
+  step3() {
+    const radios = this.income.querySelectorAll('input[type="radio"]')
+
+    radios.forEach(radio => {
+      radio.addEventListener('click', () => {
+        const isCheckedProfit = Array.from(radios).some(radio => radio.checked);
+        this.toggleBtnDisabled(this.buttonSbm, !isCheckedProfit);
+      });
+    });
+
+
+    if (Array.from(radios).some(radio => radio.checked)) {
+      this.hide(this.form)
+      this.show(this.video4)
+      this.play(this.video4);
+      this.step = 4
+      this.buttonSbm.setAttribute('step', this.step)
+      this.formHeading.textContent = "Как часто вы готовы пользоваться нейронной сетью для заработка?"
+      this.afterVideoEnd(this.video4, [this.video4, this.income], [this.form, this.howOften]);
+    } else {
+      setTimeout(() => {
+        this.buttonSbm.disabled = true;
+      }, 0);
+    }
+  }
+
+  step4() {
+    const radios = this.howOften.querySelectorAll('input[type="radio"]')
+
+    radios.forEach(radio => {
+      radio.addEventListener('click', () => {
+        const isCheckedHowOften = Array.from(radios).some(radio => radio.checked);
+        this.toggleBtnDisabled(this.buttonSbm, !isCheckedHowOften);
+      });
+    });
+
+
+    if (Array.from(radios).some(radio => radio.checked)) {
+      this.hide(this.form)
+      this.show(this.video5)
+      this.play(this.video5);
+      this.step = 5
+      this.buttonSbm.setAttribute('step', this.step)
+      this.formHeading.textContent = "Пожалуйста, введите свой электронный адрес"
+      this.afterVideoEnd(this.video5, [this.video5, this.howOften], [this.form, this.email]);
+
+      setTimeout(() => {
+        const emailErrLabel = this.form.querySelector('#email-error')
+        const eminp = this.email.childNodes[1]
+        eminp.blur();
+        eminp.removeAttribute('aria-invalid');
+        eminp.classList.remove('error');
+        emailErrLabel.remove()
+        this.email.classList.remove('error')
+      }, 0);
+
+    } else {
+      setTimeout(() => {
+        this.buttonSbm.disabled = true;
+      }, 0);
+    }
+  }
+
+  step5() {
+    const input = this.email.childNodes[1]
+    const inputs = [this.email].map((iw) => iw.childNodes[1])
+    this.setBtnColor(inputs);
+
+    if (!input.value) {
+      setTimeout(() => {
+        this.buttonSbm.disabled = true;
+      }, 0);
+
+    } else {
+      this.hide(this.form)
+      this.show(this.video6)
+      this.play(this.video6);
+      this.formHeading.textContent = "Пожалуйста введите свой номер телефона"
+      const iti = this.form.querySelector('.iti');
+      this.afterVideoEnd(this.video6, [this.video6, this.email], [this.form, iti, this.phone1]);
+
+      setTimeout(() => {
+        this.phone1.blur();
+        this.phone1.removeAttribute('aria-invalid');
+        this.phone1.classList.remove('error');
+        const phone1ErrLabel = this.form.querySelector('#phone1-error')
+        phone1ErrLabel.remove()
+      }, 0);
+
+      this.step = 6
+      this.buttonSbm.setAttribute('step', this.step)
+
+      const phone1 = document.querySelector('#phone1')
+      console.log('phone1', phone1);
+      phone1.addEventListener('keyup', (event) => {
+        setTimeout(() => {
+          const error = event.target.classList.contains('error')
+          this.toggleBtnDisabled(this.buttonSbm, error);
+        }, 0);
+
+      });
+    }
+  }
+
+  step6() {
+    if (!this.phone1.value) {
+      setTimeout(() => {
+        this.buttonSbm.disabled = true;
+      }, 0);
+
+    } else {
+      this.hide(this.form)
+      this.show(this.video7)
+      this.play(this.video7);
+      this.formHeading.textContent = "Спасибо за ваше время"
+      const iti = this.form.querySelector('.iti');
+      this.form.classList.add('final')
+      this.afterVideoEnd(this.video7, [this.video7, iti, this.phone1], [this.form]);
+
+      this.step = 7
+      this.buttonSbm.setAttribute('step', this.step)
+    }
+
   }
 
   hide(el) {
@@ -311,7 +493,10 @@ class App {
           let url = result.url;
           if (response.status === "ok") {
             if (typeof sendAnalytics !== "undefined") sendAnalytics();
-            $(form).find(".submit_btn").html("Данные отправлены успешно.");
+            $(form).find(".submit_btn").html("Перейти на сайт");
+            $(form).find(".submit_btn").on("click", function(event) {
+              window.location.href = "https://google.com";
+            });
             if (response.url) {
               setTimeout(function () {
                 window.location.href = response.url;
